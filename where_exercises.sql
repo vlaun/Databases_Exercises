6f12a2ed0be3d3d0072e59fe95e21f111d1c0ff7
Yleana Vlaun <cyvlaun4@gmail.com>
2017-01-25 11:29:24 -0600

    6.8.1 Where

---
+++ where_exercises.sql
@@ -0,0 +1,27 @@
+#Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT first_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+#Employees whose last name starts with 'E' — 7,330 rows.
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE 'E%';
+
+#Employees hired in the 90s — 135,214 rows.
+SELECT hire_date
+FROM employees
+WHERE hire_date > 2000;
+;
+
+
+#Employees born on Christmas — 842 rows.
+SELECT birth_date
+FROM employees
+WHERE birth_date LIKE '%12-25';
+
+
+#Employees with a 'q' in their last name — 1,873 rows.
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%';
