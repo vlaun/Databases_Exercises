2a52952e171e0624d5b7c1c41815a6aa2b8aa8e8
Yleana Vlaun <cyvlaun4@gmail.com>
2017-01-25 12:22:04 -0600

    6.8.2 Where Continued

--- where_exercises.sql
+++ where_exercises.sql
@@ -9,15 +9,16 @@ OR first_name = 'Maya';
 #Now add a condition to find everybody with those names who is also male — 441 rows.
 SELECT first_name
 FROM employees
-WHERE first_name = 'Irena'
+WHERE (first_name = 'Irena'
 OR first_name = 'Vidya'
-OR first_name = 'Maya'
-AND gender = 'm';
+OR first_name = 'Maya')
+AND gender = 'M';
 
 #Find all employees whose last name starts or ends with 'E' — 30,723 rows.
 SELECT  last_name
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E';
 
 
 #Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
@@ -30,5 +31,12 @@ AND last_name LIKE '%E';
 #Find all employees hired in the 90s and born on Christmas — 362 rows.
 SELECT  hire_date, birth_date
 FROM employees
-WHERE hire_date > 2000
-AND birth_date LIKE '%12-25';
+WHERE hire_date > '199%'
+AND birth_date LIKE '%-12-25';
+
+
+#Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT  last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+      AND NOT last_name LIKE '%qu%';
