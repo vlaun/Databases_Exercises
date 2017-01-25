4ea302cf29395624e63325549e2e12171bcc8902
Yleana Vlaun <cyvlaun4@gmail.com>
2017-01-25 16:07:46 -0600

    6.9 Functions

---
+++ functions_exercises.sql
@@ -0,0 +1,42 @@
+#Update your query for 'Irena', 'Vidya', or 'Maya'.
+# Use count(*) and GROUP BY to find the number of employees for each gender
+# with those names.
+SELECT COUNT(first_name)
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+#ORDER BY first_name
+GROUP BY gender = 'F', 'M';
+
+
+#Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.
+#SELECT first_name, last_name
+#FROM employees
+#WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+#ORDER BY first_name, last_name;
+
+#Change the order by clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
+#SELECT last_name, first_name
+#FROM employees
+#WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+#ORDER BY last_name, first_name;
+
+
+#Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!
+#SELECT last_name, emp_no
+#FROM employees
+#WHERE last_name LIKE '%E%'
+#ORDER BY emp_no;
+
+
+#Now reverse the sort order for both queries.
+#SELECT last_name, emp_no
+#FROM employees
+#WHERE last_name LIKE '%E%'
+#ORDER BY emp_no DESC;
+
+#Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
+#SELECT first_name, last_name, hire_date, birth_date, emp_no
+#FROM employees
+#WHERE birth_date LIKE '%-12-25'
+#      AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+#ORDER BY birth_date, hire_date DESC;
