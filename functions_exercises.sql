0451df03f0c4162914afbff8102f880b5c5f7006
Yleana Vlaun <cyvlaun4@gmail.com>
2017-01-25 16:36:08 -0600

    6.9 Functions

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,42 +1,43 @@
 #Update your query for 'Irena', 'Vidya', or 'Maya'.
 # Use count(*) and GROUP BY to find the number of employees for each gender
 # with those names.
-SELECT COUNT(first_name)
+SELECT COUNT(*), gender as 'Number of Male and Female employees'
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 #ORDER BY first_name
-GROUP BY gender = 'F', 'M';
+GROUP BY gender;
 
 
-#Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.
-#SELECT first_name, last_name
-#FROM employees
-#WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-#ORDER BY first_name, last_name;
+#Update your queries for employees whose names start and end with 'E'.
+# Use concat() to combine their first and last name together
+# as a single column in your results.
 
-#Change the order by clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
-#SELECT last_name, first_name
-#FROM employees
-#WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-#ORDER BY last_name, first_name;
+SELECT CONCAT(first_name, " ", last_name) as 'full name'
+FROM employees
+WHERE last_name LIKE 'E%E'
+ORDER BY emp_no;
 
 
-#Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!
-#SELECT last_name, emp_no
-#FROM employees
-#WHERE last_name LIKE '%E%'
-#ORDER BY emp_no;
+#For your query of employees born on Christmas and hired in the 90s,
+# use datediff() to find how many days they have been working at the company
+# (Hint: You will also need to use now() or curdate())
+
+SELECT first_name, last_name, DATEDIFF(now(), hire_date) as 'Number of days employed'
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+AND hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
 
-#Now reverse the sort order for both queries.
-#SELECT last_name, emp_no
-#FROM employees
-#WHERE last_name LIKE '%E%'
-#ORDER BY emp_no DESC;
 
-#Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
-#SELECT first_name, last_name, hire_date, birth_date, emp_no
-#FROM employees
-#WHERE birth_date LIKE '%-12-25'
-#      AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-#ORDER BY birth_date, hire_date DESC;
+#Add a GROUP BY clause to your query for last names with 'q' and not 'qu'
+# to find the distinct combination of first and last names.
+# You will find there were some duplicate first and last name pairs
+# in your previous results. Add a count() to your results and use ORDER BY
+# to make it easier to find employees whose unusual name is shared with others.
+
+SELECT CONCAT(first_name, " ", last_name) as "full_name", count(first_name) as "number of people with this name"
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY full_name
+ORDER BY 'number of people with this name' DESC;
