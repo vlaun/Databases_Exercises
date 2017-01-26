2fbe4829342b5264dfe0cb87656db57937391e01
Yleana Vlaun <cyvlaun4@gmail.com>
2017-01-26 10:59:33 -0600

    6.10 Aliases

---
+++ employees.sql
@@ -0,0 +1,23 @@
+SELECT * FROM employees;
+
+SELECT * FROM departments;
+
+
+#Return 10 employees in a result set named "full_name"
+# in the format of "lastname, firstname" for each employee.
+SELECT CONCAT(last_name, ' ', first_name) AS full_name
+FROM employees
+GROUP BY full_name
+LIMIT 10;
+
+#Add the date of birth for each employee as "DOB" to the query.
+SELECT CONCAT(first_name, ' ', last_name) AS full_name, birth_date AS DOB
+FROM employees
+LIMIT 10;
+
+
+# Update the query to format full name to include the employee number
+# so it is formatted as "employee number - lastname, firstname".
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS last_name, first_name
+FROM employees
+LIMIT 10;
