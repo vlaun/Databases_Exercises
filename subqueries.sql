48d4f85b6dab3e758ccbbad2cffe78c1fafca666
Yleana Vlaun <cyvlaun4@gmail.com>
2017-01-27 12:08:57 -0600

    6.11.3 Sub-queries

--- subqueries.sql
+++ subqueries.sql
@@ -10,18 +10,17 @@ WHERE hire_date IN (
 );
 
 #Find all the titles held by all employees with the first name Aamod.
-SELECT title, first_name, last_name
-FROM employees
-  JOIN titles
-WHERE title IN (
-  SELECT title
-  FROM titles
-  WHERE first_name  like "Aamod"
+SELECT *
+FROM titles
+WHERE emp_no IN (
+  SELECT emp_no
+  FROM employees
+  WHERE first_name = "Aamod"
 );
 
 
 #Find all the department managers that are female.
-SELECT gender, first_name, last_name
+SELECT *
 FROM employees as managers
   JOIN dept_manager
 WHERE gender IN (
@@ -32,7 +31,7 @@ WHERE gender IN (
 
 
 #Find all the department names that have female managers.
-SELECT dept_name as "Departments managed by women.", gender, first_name, last_name
+SELECT *
 FROM departments
   JOIN dept_manager on dept_manager.dept_no = departments.dept_no
   JOIN employees as managers on dept_manager.emp_no = managers.emp_no
